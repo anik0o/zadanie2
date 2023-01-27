4e3e934943f4b066e4947ff46912984c32d79a0a
anik0o <weronika.mr2@gmail.com>
2023-01-27 18:18:53 +0100

    Zadanie2 fill with files

---
+++ client/src/OtherPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default () => {
+  return (
+    <div>
+      
+      <Link to="/">Go back to MULTIFIB</Link>
+      <div>
+      MULTIFIB
+      Weronika Wach
+      GR 2.3
+      </div>
+    </div>
+  );
+};
